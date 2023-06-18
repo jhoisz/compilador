b1ec8a61a5957ca60a48a047fdea81802d28c7b6
jhoisz <jvrda@ufpi.edu.br>
2023-06-18 20:59:31 -0300

    Tratamento do input

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -102,8 +102,9 @@ class _HomeState extends State<Home> {
                       IconButton(
                         onPressed: () async {
                           if (_codeController != null) {
-                            final String input = _codeController!.text;
+                            String input = _codeController!.text;
                             if (input != '') {
+                              input = input.replaceAll('\r', ' ');
                               final response = await api.postCommand(input);
 
                               setState(() {
@@ -132,7 +133,7 @@ class _HomeState extends State<Home> {
                   Text(
                     terminal,
                     style: TextStyle(
-                        fontWeight: FontWeight.bold, 
+                        fontWeight: FontWeight.bold,
                         fontSize: 16.0,
                         color: isError ? MyColors.success : MyColors.error),
                   )
